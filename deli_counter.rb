258d2cb4de93ee59d58a7926b5f070f079a5c59b
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 20:30:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = []
+
+def line(other_deli)
+  if other_deli.size == 0
+    puts "The line is currently empty."
+  else
+    line_message = "The line is currently:"
+    other_deli.each_with_index do |name, index|
+      line_message += " #{index.to_i + 1}. #{name}"
+    end
+    puts line_message
+  end
+end
+
+
+def take_a_number(katz_deli, "Ada")
+  if
+end
+
+
